6b3b156f5ca456cf29b9619faa3e91c5475534b7
TomoakiMacBookAirM1 <tomoaki.nagahara@gmail.com>
2024-03-13 20:07:13 +0900

    New: BITCOIN_CLI.trait.php

---
+++ BITCOIN_CLI.trait.php
@@ -0,0 +1,32 @@
+<?php
+/** op-unit-bitcoin:/Bitcoin-CLI.trait.php
+ *
+ * @created    2024-03-13
+ * @version    1.0
+ * @package    op-unit-bitcoin
+ * @author     Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright  Tomoaki Nagahara All right reserved.
+ */
+
+/** Declare strict
+ *
+ */
+declare(strict_types=1);
+
+/** namespace
+ *
+ */
+namespace OP\UNIT\BITCOIN;
+
+/** Bitcoin CLI trait
+ *
+ * @created    2024-03-13
+ * @version    1.0
+ * @package    op-unit-bitcoin
+ * @author     Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright  Tomoaki Nagahara All right reserved.
+ */
+trait BITCOIN_CLI
+{
+
+}
